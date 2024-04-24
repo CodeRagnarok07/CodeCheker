cf70920d40442557b84bd98f8d37141ab907aef3
win desktop <angelriera1796@gmail.com>
2024-04-24 10:13:09 -0400

    x

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-
+import fs from 'node:fs/promises'
 
 
 const webs: string[] = [
@@ -18,25 +18,34 @@ const discount = [
     "Add discount code",
     "Discount code"
 ];
+const curretn = "https://cynthiarowley.com/en-ve/collections/tops/products/butterfly-applique-top-1?variant=42430828675252";
 
+const getPageToLocal = async (web: string) => {
+    const req = await fetch(web)
+    const body = await req.text()
+    fs.writeFile('./test.html', body, { encoding: 'utf8' },)
+};
 
+const webScraping = async () => {
+    const FileData = await fs.readFile('./test.html', { encoding: 'utf8' }, )
+
+    const regex = /<([^>]+?)>.*?\${Bulgaria (BGN лв.)}.*?<\/\1>/g;
+    console.log(FileData.match(regex))
+
+}
+
+// write a regext that get any node element with a text ${text} inside
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 400
-    });
-    const page = await browser.newPage();
-
+    // const browser = await puppeteer.launch({
+    //     headless: false,
+    //     slowMo: 400
+    // });
+    // const page = await browser.newPage();
     // Navigate the page to a URL
-    await page.goto("https://cynthiarowley.com/en-ve/collections/tops/products/butterfly-applique-top-1?variant=42430828675252");
+    // await page.goto();
+    await webScraping()
 
-    const btnAddCart = await page.waitForSelector(
-        'text/add to cart'
-    );
-    console.log(btnAddCart)
-
-
-    await browser.close();
+    // await browser.close();
 })();
